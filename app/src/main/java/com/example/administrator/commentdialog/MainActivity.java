827650c934914eb231b8f74f042fe082c84ff24e
王会林 <wanghuilin@imopan.com>
2016-06-08 14:41:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/commentdialog/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.administrator.commentdialog;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.administrator.commentdialog.comment.CommentDialog;
+import com.example.administrator.commentdialog.comment.CommentView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CommentView commentView;
+    CommentDialog commentDialog;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (commentDialog != null) {
+                commentDialog.cancel();
+                Toast.makeText(MainActivity.this, "评论成功~", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        commentView = (CommentView) findViewById(R.id.comment_view);
+        commentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                commentDialog = new CommentDialog(MainActivity.this).setListener(new CommentDialog.Listener() {
+                    @Override
+                    public void commentContent(String content) {
+//                        TODO 发表评论 发表成功后取消Dialog
+                        handler.sendEmptyMessageDelayed(1, 2000);
+
+                    }
+                });
+                commentDialog.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/administrator/commentdialog/comment/CommentView.java
@@ -0,0 +1,25 @@
+package com.example.administrator.commentdialog.comment;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.administrator.commentdialog.R;
+
+/**
+ * @author wanghuilin
+ * @time 2016/5/31  10:29
+ * <p/>
+ * 详情评论视图
+ */
+public class CommentView extends LinearLayout {
+    public CommentView(Context context) {
+        this(context, null);
+    }
+
+    public CommentView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        View.inflate(context, R.layout.comment_view_layout, this);
+    }
+}
